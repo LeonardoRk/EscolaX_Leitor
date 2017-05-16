070e21b1ee6e33e8a1e8f7d219633b699f2be984
FilipeKN4 <filipekn4@gmail.com>
2017-05-15 23:01:49 -0300

    Changes method of search alumn by bar code.
    Signed-off-by: FilipeKN4 <filipekn4@gmail.com>
    Signed-off-by: LeonardoRk <leodegolim@yahoo.com.br>

--- Escola X Leitor/app/controllers/reader_controller.rb
+++ Escola X Leitor/app/controllers/reader_controller.rb
@@ -4,15 +4,6 @@ class ReaderController < ApplicationController
 
 		if bar_code != nil && bar_code != ""
 
-			alumn_with_equal_bar_code = Alumn.find_by bar_code: bar_code
-			
-			if alumn_with_equal_bar_code != nil
-				@title = alumn_with_equal_bar_code[:address] + " " + alumn_with_equal_bar_code[:name]
-				#@title = "Value: " + bar_code
-			else
-				# nothing to do in here
-			end
-
 			@alumn = Alumn.find_by bar_code: bar_code
 
 		else
